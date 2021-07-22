3086e8b256f0f6cb7d18390ba5a1568af32a0b29
AnZQ <anzhongqi@marknum.com>
2021-07-22 10:37:41 +0800

    angular

---
+++ micros-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
