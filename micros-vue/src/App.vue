7fa76b08dfbe82929fc9851272b56c4e7a6c89a9
AnZQ <anzhongqi@marknum.com>
2021-07-22 10:44:04 +0800

    vue

---
+++ micros-vue/src/App.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Desc: 
+ * @Author: 安忠琪
+ * @LastEditTime: 2021-07-21 11:09:09
+-->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ micros-vue/src/components/luomao.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Desc: 
+ * @Author: 安忠琪
+ * @LastEditTime: 2021-07-13 17:40:07
+-->
+<template>
+  <div class="contanier">
+    <div class="neumorphism"></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.contanier {
+  width: 200px;
+  height: 200px;
+  background: #a86161;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+}
+.contanier .neumorphism {
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background: #a86161;
+  box-shadow: inset 20px 20px 60px #8f5252, inset -20px -20px 60px #c17070;
+}
+</style>
